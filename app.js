1fcb1670c5036cc4cee8f2da188cc12c30af41e4
Ellen Lee <leechinghui.tw@gmail.com>
2019-08-30 15:08:56 +0800

    add home and user routers

--- app.js
+++ app.js
@@ -8,7 +8,6 @@ const methodOverride = require('method-override')
 
 const db = require('./models')
 const Todo = db.Todo
-const User = db.User
 
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
@@ -20,33 +19,8 @@ app.use(methodOverride('_method'))
 app.get('/', (req, res) => {
   res.send('hello world')
 })
-// 認證系統的路由
-// 登入頁面
-app.get('/users/login', (req, res) => {
-  res.render('login')
-})
-// 登入檢查
-app.post('/users/login', (req, res) => {
-  res.send('login')
-})
-// 註冊頁面
-app.get('/users/register', (req, res) => {
-  res.render('register')
-})
-// 註冊檢查
-app.post('/users/register', (req, res) => {
-  User.create({
-    name: req.body.name,
-    email: req.body.email,
-    password: req.body.password
-  }).then(user => res.redirect('/'))
-})
-// 登出
-app.get('/users/logout', (req, res) => {
-  res.send('logout')
-})
+app.use('/users', require('./routes/user'))
 
-// 設定 express port 3000
 app.listen(port, () => {
   console.log(`App is running on port ${port}!`)
 })
