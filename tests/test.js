37dc9e9cd9252ecaba9992b5a2fd34ac6d003baf
v123582 <v123582@gmail.com>
2019-08-30 19:32:40 +0800

    [ ] 比較斷言庫的不同

--- tests/test.js
+++ tests/test.js
@@ -1,9 +1,19 @@
 const assert = require('assert')
+const chai = require('chai')
+const should = chai.should()
+
 
 describe('Array', function() {
   describe('#indexOf()', function() {
     it('should return -1 when the value is not present', function() {
-      assert.equal([1, 2, 3].indexOf(4), -1);
+
+      const data = [1, 2, 3].indexOf(4)
+
+      // 比較斷言庫的不同
+      assert.equal(data, -1)
+      chai.assert.equal(data, -1)
+      chai.expect(data).to.equal(-1)
+      data.should.equal(-1)
     });
   });
 });
